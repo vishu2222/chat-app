a6b1a199b2a0813ee32358a94ac3516d155d198e
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-21 22:54:17 +0530

    moved socket outside component function

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import { io } from 'socket.io-client'
+// import { useState } from 'react';
 import { ChatBox } from './components/ChatBox';
+import { DisplayMsg } from "./components/DisplayMsg";
+
+const socket = io.connect('http://localhost:3000') // const socket = io('http://localhost:3000')
 
 function App() {
-  const socket = io('http://localhost:3000')
+
+  // const [userName, setUserName] = useState('')
+  // const [room, setRoom] = useState('')
 
   socket.on('connect', () => {
     console.log('connected with id', socket.id)
   })
 
-  function sendMessage(chatTxt) {
-    socket.emit('newMessage', chatTxt)
+  function sendMessage(newChatTxt) {
+    socket.emit('newMessage', newChatTxt)
   }
 
   socket.on('newBroadcast', (msg) => {
@@ -20,8 +26,16 @@ function App() {
   return (
     <div className="App" id='app-container'>
       <ChatBox msg={sendMessage} />
+      <DisplayMsg msg={newMsg} />
     </div>
   );
 }
 
 export default App;
+
+
+
+// <h2>Join A chat</h2>
+//       <input type="text" placeholder='User Name' onChange={(e) => setUserName(e.target.value)} />
+//       <input type="text" placeholder='Room' onChange={(e) => setRoom(e.target.value)} />
+//       <button>Join</button>
