df5281f7e3de30b8fdc1704105455a31e137a5bf
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-21 14:42:40 +0530

    moved ui part to component chatBox

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,25 @@
 import './App.css';
 import { io } from 'socket.io-client'
-import { useState } from 'react'
-import { Message } from './components/Message.jsx';
+import { ChatBox } from './components/ChatBox';
 
 function App() {
-  const [chatTxt, setChatTxt] = useState('')
-  const [messages, setMessages] = useState([])
   const socket = io('http://localhost:3000')
 
   socket.on('connect', () => {
     console.log('connected with id', socket.id)
   })
 
-  socket.on('msgFromServer', (msg) => {
-    console.log('recived from server:', msg)
-    const id = Math.max(messages.map(msg => msg.id)) + 1 || 0
-    setMessages([...messages, { id: id, msg: msg }])
-  })
-
-  function sendMessage() {
-    socket.emit('message', chatTxt)
-    setChatTxt('')
+  function sendMessage(chatTxt) {
+    socket.emit('newMessage', chatTxt)
   }
+
+  socket.on('newBroadcast', (msg) => {
+    console.log('BroadCast from server:', msg)
+  })
 
   return (
     <div className="App" id='app-container'>
-      <div id='message-container'>
-        {messages.map(item => <Message msg={item.msg} key={item.id} />)}
-      </div>
-      <div id='form-div'>
-        <form id='from' onSubmit={(e) => e.preventDefault()}>
-          <input id='input'
-            placeholder='...'
-            value={chatTxt}
-            onChange={(e) => setChatTxt(e.target.value)}>
-          </input>
-          <button onClick={sendMessage}>send</button>
-        </form>
-      </div>
+      <ChatBox msg={sendMessage} />
     </div>
   );
 }
