7990b59217043e1033c82780d8e97dd0135b83ff
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-22 22:23:22 +0530

    added functions joinUser, emit newMessage

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,31 @@
 import './App.css';
-import { io } from 'socket.io-client'
+import { io } from 'socket.io-client';
 import { useState } from 'react';
-import { ChatBox } from './components/ChatBox';
-import { JoinRoom } from './components/JoinRoom';
-// import { DisplayMsg } from "./components/DisplayMsg";
+import { Home } from './components/Home';
+import { MsgBox } from './components/MsgBox';
 
-const socket = io.connect('http://localhost:3000') // const socket = io('http://localhost:3000')
+const socket = io.connect('http://localhost:3000')
 
 function App() {
+  socket.on('connect', () => { console.log('connected with id:', socket.id) })
 
+  // state
   const [userJoined, setUserJoined] = useState(false)
+  const [userName, setUserName] = useState('')
+  const [room, setRoom] = useState('')
 
-  socket.on('connect', () => {
-    console.log('connected with id', socket.id)
-  })
-
-  function sendNewMessage(newChatTxt) {
-    socket.emit('newMessage', newChatTxt)
+  // methods
+  function assignUser(userName, room) {
+    setUserName(userName)
+    setRoom(room)
   }
 
-  socket.on('newBroadcast', (msg) => {
-    console.log('BroadCast from server:', msg)
-  })
-
-  function joinUser(userName, room) {
-    console.log(userName, room)
-    socket.emit('joinRoom', { userName: userName, room: room })
-    setUserJoined(true)
-  }
-
+  //  component return
   return (
-    <div className="App" id='app-container'>
-      {!userJoined && <JoinRoom joinUser={joinUser} />}
-      {userJoined && <ChatBox sendNewMessage={sendNewMessage} />}
-    </div>
-  );
+    <div>
+      {!userJoined && <Home socket={socket} setUserJoined={setUserJoined} assignUser={assignUser} />}
+      {userJoined && <MsgBox socket={socket} userName={userName} room={room} />}
+    </div>)
 }
 
 export default App;
-
-
-
-// <h2>Join A chat</h2>
-//       <input type="text" placeholder='User Name' onChange={(e) => setUserName(e.target.value)} />
-//       <input type="text" placeholder='Room' onChange={(e) => setRoom(e.target.value)} />
-//       <button>Join</button>
