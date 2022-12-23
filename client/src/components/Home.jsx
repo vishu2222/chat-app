f4fac520d764cd16182718e9bf64340474e538c1
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-23 09:27:28 +0530

    added form and constraint for userName, room

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,14 @@
 import { useState, useEffect } from "react";
 
-export function Home({ socket, setUserJoined, assignUser }) {
+export function Home({ socket, setUserStatus, assignUser }) {
   const [userName, setUserName] = useState("");
   const [room, setRoom] = useState("");
 
   function handleClick() {
-    socket.emit("joinRoom", { userName: userName, room, room });
+    if (userName === "" || room === "") return;
+    socket.emit("joinRoom", { userName: userName, room: room });
     assignUser(userName, room);
-    setUserJoined(true);
+    setUserStatus(true);
     setUserName("");
     setRoom("");
   }
@@ -16,29 +17,32 @@ export function Home({ socket, setUserJoined, assignUser }) {
     socket.on("userJoined", (data) => {
       console.log(data);
     });
-  }, []);
+  }, [socket]);
 
   // component return
   return (
-    <div id="Home">
-      <h2>Join Chat</h2>
-      <label>
-        <strong>UserName:</strong>
-      </label>
-      <input
-        type="text"
-        value={userName}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <label>
-        <strong>Room:</strong>
-      </label>
-      <input
-        type="text"
-        value={room}
-        onChange={(e) => setRoom(e.target.value)}
-      />
-      <button onClick={handleClick}>Join</button>
+    <div id="homeDiv">
+      <form onSubmit={(e) => e.preventDefault()}>
+        <h2> Join Chat</h2>
+        <label>
+          <strong> User Name: </strong>
+        </label>
+        <input
+          type="text"
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+
+        <label>
+          <strong> Room: </strong>
+        </label>
+        <input
+          type="text"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
+        <button onClick={handleClick}>Join</button>
+      </form>
     </div>
   );
 }
