41a2815b784cd21413db74b94062785d2adda552
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-23 09:25:54 +0530

    moved socket.on('connect') to inside useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
-import './App.css';
+import './App.css'
 import { io } from 'socket.io-client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Home } from './components/Home';
 import { MsgBox } from './components/MsgBox';
 
 const socket = io.connect('http://localhost:3000')
 
 function App() {
-  socket.on('connect', () => { console.log('connected with id:', socket.id) })
-
   // state
-  const [userJoined, setUserJoined] = useState(false)
+  const [userJoinStatus, setUserStatus] = useState(false)
   const [userName, setUserName] = useState('')
   const [room, setRoom] = useState('')
 
@@ -20,11 +18,15 @@ function App() {
     setRoom(room)
   }
 
+  useEffect(() => {
+    socket.on('connect', () => { console.log('new user connected with id:', socket.id) })
+  }, [])
+
   //  component return
   return (
-    <div>
-      {!userJoined && <Home socket={socket} setUserJoined={setUserJoined} assignUser={assignUser} />}
-      {userJoined && <MsgBox socket={socket} userName={userName} room={room} />}
+    <div id='appContainer'>
+      {!userJoinStatus && <Home socket={socket} setUserStatus={setUserStatus} assignUser={assignUser} />}
+      {userJoinStatus && <MsgBox socket={socket} userName={userName} room={room} />}
     </div>)
 }
 
