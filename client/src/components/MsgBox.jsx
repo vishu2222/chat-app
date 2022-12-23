397321a1c722cec561fba508c579cca93bef3dcc
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-23 11:13:19 +0530

    prettier ' '  to  " "

--- client/src/components/MsgBox.jsx
+++ client/src/components/MsgBox.jsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import { MessageItem } from "./MessageItem";
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { MessageItem } from './MessageItem';
 
 export function MsgBox({ socket, userName, room }) {
-  const [newMessage, setNewMessage] = useState("");
+  const [newMessage, setNewMessage] = useState('');
   const [messageList, setMessageList] = useState([]);
 
   // methods
@@ -12,38 +12,42 @@ export function MsgBox({ socket, userName, room }) {
       userName: userName,
       room: room,
       txt: newMessage,
-      date: new Date(),
+      date: new Date()
     };
     setMessageList([...messageList, newMsg]);
-    socket.emit("newMessage", newMsg);
-    setNewMessage("");
+    socket.emit('newMessage', newMsg);
+    setNewMessage('');
   }
 
   useEffect(() => {
     const callBack = (data) => {
-      console.log("broadCast", data.userName, data.txt);
+      console.log('broadCast', data.userName, data.txt);
       setMessageList((currentList) => [...currentList, data]);
     };
-    socket.on("broadcastMsg", callBack);
+    socket.on('broadcastMsg', callBack);
     return () => {
-      socket.off("broadcastMsg", callBack);
+      socket.off('broadcastMsg', callBack);
     };
-  }, []);
+  }, [socket]);
 
   // component return
   return (
     <div id="messageBox">
       <h2>You are inside Room: {room}</h2>
-      <input
-        type="text"
-        placeholder="...."
-        value={newMessage}
-        onChange={(e) => setNewMessage(e.target.value)}
-      />
-      <button onClick={handleClick}>send</button>
+
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          type="text"
+          placeholder="...."
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+        />
+        <button onClick={handleClick}>send</button>
+      </form>
+
       <div id="message-container">
         {messageList.map((msg, index) => (
-          <MessageItem msg={msg} key={index} />
+          <MessageItem msg={msg} key={index} userName={userName} />
         ))}
       </div>
     </div>
