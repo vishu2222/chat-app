5afb283740a5467f266acbf70f9353ae60fe7475
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-26 23:15:57 +0530

    added rout for Register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Login';
+import { Register } from './pages/Register';
 
 function App() {
   return (
     <div id='appContainer'>
       <BrowserRouter>
-        <div id='nav'>
-          <Link to='/'>Home</Link>
-        </div>
+        <div id='nav'></div>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='*' element={<h1>Page not found</h1>} />
         </Routes>
       </BrowserRouter>
     </div>)
