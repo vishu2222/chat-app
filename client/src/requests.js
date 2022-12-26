7b873fb2bb5317ef560e348399a6821c219429a5
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-26 23:16:49 +0530

    add user-SignUp, checkUserNameExist

---
+++ client/src/requests.js
@@ -0,0 +1,15 @@
+
+const url = 'http://localhost:3000/'
+
+export async function checkUserNameExists(userName) {
+    const res = await fetch(url + 'checkUser', {
+        headers: { "Content-Type": "application/json" },
+        method: "POST",
+        body: JSON.stringify({ userName })
+    });
+    return await res.json()
+}
+
+export async function signupUser(userName, password) {
+    const res = await fetch()
+}
