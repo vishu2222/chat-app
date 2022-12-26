3454d2c835caba565b7f25a08cf32086854a7a8e
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-26 13:06:18 +0530

    remove semi-colons

--- client/src/components/MessageItem.jsx
+++ client/src/components/MessageItem.jsx
@@ -1,16 +1,18 @@
 export function MessageItem({ msg, userName }) {
-  let divClassName = '';
-  let msgClassName = '';
+  let divClassName = ''
+  let msgClassName = ''
+
   if (msg.userName === userName) {
-    divClassName = 'self-msg-div';
-    msgClassName = 'self-msg';
+    divClassName = 'self-msg-div'
+    msgClassName = 'self-msg'
   } else {
-    divClassName = 'user-msg-div';
-    msgClassName = 'user-msg';
+    divClassName = 'user-msg-div'
+    msgClassName = 'user-msg'
   }
+
   return (
     <div className={divClassName}>
       <p className={msgClassName}>{msg.userName + ': ' + msg.txt}</p>
     </div>
-  );
+  )
 }
