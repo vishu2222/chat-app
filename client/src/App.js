421e3209bfeac529d4968987d522c30005ad92de
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-27 13:32:25 +0530

    add roue to chat page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Login';
 import { Register } from './pages/Register';
+import { Chat } from './pages/Chat'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/chat' element={<Chat />} />
           <Route path='*' element={<h1>Page not found</h1>} />
         </Routes>
       </BrowserRouter>
