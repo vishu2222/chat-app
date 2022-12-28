ebd82d63e337ea98c015da22975d20c126d6a983
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-28 16:16:59 +0530

    notes update

--- docs/bcrypt.js
+++ docs/bcrypt.js
@@ -1,30 +1,28 @@
-
-
 app.post('/users', async (req, res) => {
-    try {
-        const salt = await bcrypt.genSalt()
-        const hasherPassword = await bcrypt.hash(req.body.password, salt)
-        const user = { name: req.body.name, password: hasherPassword }
-        users.push(user)
-        res.sendStatus(200)
-    } catch {
-        res.sendStatus(500)
-    }
+  try {
+    const salt = await bcrypt.genSalt()
+    const hasherPassword = await bcrypt.hash(req.body.password, salt)
+    const user = { name: req.body.name, password: hasherPassword }
+    users.push(user)
+    res.sendStatus(200)
+  } catch {
+    res.sendStatus(500)
+  }
 })
 
 app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name = req.body.name)
-    if (user === null) return res.sendStatus(400)
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.sendStatus(200).send('success')
-        } else {
-            res.send('not allowed')
-        }
-    } catch {
-
+  const user = users.find((user) => (user.name = req.body.name))
+  if (user === null) return res.sendStatus(400)
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.sendStatus(200).send('success')
+    } else {
+      res.send('not allowed')
     }
+  } catch {}
 })
 
+// salt rounds read
+// https://stackoverflow.com/questions/46693430/what-are-salt-rounds-and-how-are-salts-stored-in-bcrypt
 
 // https://openbase.com/js/bcrypt/documentation
