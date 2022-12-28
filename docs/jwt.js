caa526d3a345580503d442f45708da5234bb3278
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-28 16:16:45 +0530

    notes comments

--- docs/jwt.js
+++ docs/jwt.js
@@ -7,44 +7,51 @@ const jwt = require('jsonwebtoken')
 app.use(express.json())
 
 app.get('/posts', authenticateToken, (req, res) => {
-    res.json(posts.filter(post => post.userName === req.user.name))
+  res.json(posts.filter((post) => post.userName === req.user.name))
 })
 
 app.post('/login', (req, res) => {
-    // authenticate
+  // authenticate
 
-    const userName = req.body.userName;
-    const user = { user: userName }
+  const userName = req.body.userName
+  const user = { user: userName }
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json({ accessToken: accessToken })
+  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+  res.json({ accessToken: accessToken })
 })
 
-
 function authenticateToken(req, res, next) {
-    const authHeadder = req.headders['authorization']
-    const token = authHeadder && authHeadder.split(' ')[1]
-    if (token === null) return res.sendStatus(401)
+  const authHeadder = req.headders['authorization']
+  const token = authHeadder && authHeadder.split(' ')[1]
+  if (token === null) return res.sendStatus(401)
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) res.sendStatus(403)
-        req.user = user
-        next()
-    })
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) res.sendStatus(403)
+    req.user = user
+    next()
+  })
 }
 
 app.listen(3000)
 
-
+// app.use(function (req, res, next) {
+//   res.header('Content-Type', 'application/json;charset=UTF-8')
+//   res.header('Access-Control-Allow-Credentials', true)
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept'
+//   )
+//   next()
+// })
 
 // requests
 
-POST http://localhost:3000/login
-Content - Type: application / json
-{
-    "userName": "user1"
-}
+// POST http://localhost:3000/login
+// Content - Type: application / json
+// {
+//     "userName": "user1"
+// }
 
-GET http://localhost:3000/posts
-Authorization: Bearer
-xxxxx.yyyyyy.zzzzzz
+// GET http://localhost:3000/posts
+// Authorization: Bearer
+// xxxxx.yyyyyy.zzzzzz
