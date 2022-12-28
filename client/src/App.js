674470856882cab7f1c96ee61c07ca0309d122ff
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-28 22:44:44 +0530

    Change Home to Login, / to /login, add Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Login';
-import { Register } from './pages/Register';
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
 import { Chat } from './pages/Chat'
+import { Home } from './pages/Home'
 
 function App() {
   return (
-    <div id='appContainer'>
+    <div id="appContainer">
       <BrowserRouter>
-        <div id='nav'></div>
+        <div id="nav"></div>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/chat/:userName' element={<Chat />} />
-          <Route path='*' element={<h1>Page not found</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/chat/:userName" element={<Chat />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
         </Routes>
       </BrowserRouter>
-    </div>)
+    </div>
+  )
 }
 
-export default App;
+export default App
