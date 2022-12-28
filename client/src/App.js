2be2aea2168a3f9e0955bd59c53a49f7ebbb17f3
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-29 01:35:12 +0530

    add nav-home-button

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
 import { Chat } from './pages/Chat'
@@ -7,18 +7,23 @@ import { Home } from './pages/Home'
 
 function App() {
   return (
-    <div id="appContainer">
-      <BrowserRouter>
-        <div id="nav"></div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/chat/:userName" element={<Chat />} />
-          <Route path="*" element={<h1>Page not found</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <>
+      <div id="appContainer">
+        <BrowserRouter>
+          <div id="nav-home-button">
+            <Link to="/">Home</Link>
+          </div>
+          <div id="nav"></div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/chat/:userName" element={<Chat />} />
+            <Route path="*" element={<h1>Page not found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   )
 }
 
