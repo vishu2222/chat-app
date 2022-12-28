919341c6bdbbdfac9e0da2c563bcb0bda6874688
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-28 16:18:06 +0530

    add function getTocken

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,30 +1,36 @@
-
 const url = 'http://localhost:3000/'
 
 export async function checkUserNameExists(userName) {
-    const res = await fetch(url + 'checkUser', {
-        headers: { 'Content-Type': 'application/json' },
-        method: "POST",
-        body: JSON.stringify({ userName })
-    });
-    return await res.json()
+  const res = await fetch(url + 'checkUser', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({ userName })
+  })
+  return await res.json()
 }
 
 export async function signupUser(userName, password) {
-    const res = await fetch(url + 'signUp', {
-        headers: { 'Content-Type': 'application/json' },
-        method: "POST",
-        body: JSON.stringify({ userName, password })
-    })
-    return res.status
+  const res = await fetch(url + 'signUp', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({ userName, password })
+  })
+  return res.status
+}
+
+export async function getToken(userName) {
+  const res = await fetch(url + 'getToken', { credentials: 'include' }) // include: allow send or recieve cookies for cross-origin requists
+  //   const cookies = new Cookies()
+  //   console.log(cookies.get('token'))
 }
 
 export async function userLogin(userName, password) {
-    const res = await fetch(url + 'signIn', {
-        headers: { 'Content-Type': 'application/json' },
-        method: "POST",
-        body: JSON.stringify({ userName, password })
-    })
-    const status = res.status
-    return status
+  const res = await fetch(url + 'login', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({ userName, password })
+  })
+  const status = res.status
+  //   console.log(status)
+  return status
 }
