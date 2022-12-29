0ba481b0fbb719be9bf0c2187970c04ca7f947a6
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-29 12:26:53 +0530

    code restructure

--- server/db/queries.js
+++ server/db/queries.js
@@ -2,34 +2,41 @@ import pg from 'pg'
 
 const { Pool } = pg
 const pool = new Pool({
-    user: 'vishu',
-    host: 'localhost',
-    database: 'chatdb',
-    password: '12345',
-    port: 5432
+  user: 'vishu',
+  host: 'localhost',
+  database: 'chatdb',
+  password: '12345',
+  port: 5432
 })
 
 export async function checkUserNameExists(userName) {
-    const client = await pool.connect()
-    const res = await client.query(`SELECT user_name from users WHERE user_name ='${userName}';`)
-    client.release()
-    if (res.rowCount > 0) {
-        return true
-    }
-    return false
+  const client = await pool.connect()
+  const res = await client.query(
+    `SELECT user_name from users WHERE user_name ='${userName}';`
+  )
+  client.release()
+  if (res.rowCount > 0) {
+    return true
+  }
+  return false
 }
 
 export async function signUp(userName, password) {
-    const client = await pool.connect()
-    const res = await client.query(`INSERT INTO users(user_id, user_name, password) 
+  const client = await pool.connect()
+  const res =
+    await client.query(`INSERT INTO users(user_id, user_name, password) 
     VALUES (NEXTVAL('user_id_seq'), '${userName}', '${password}')`)
-    client.release()
-    if (res.rowCount < 1) throw Error
+  client.release()
+  if (res.rowCount < 1) throw Error
 }
 
 export async function getPassword(userName) {
-    const client = await pool.connect()
-    const res = await client.query(`SELECT password FROM users where user_name = '${userName}'`)
-    client.release()
-    return await res.rows[0].password
+  const client = await pool.connect()
+  const res = await client.query(
+    `SELECT password FROM users where user_name = '${userName}'`
+  )
+  client.release()
+  return await res.rows[0].password
 }
+
+// INSERT INTO rooms(room_id,room) VALUES ((nextval('room_id_seq')),'demo');
