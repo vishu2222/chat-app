afa5fbe68dcc411d299b7b3502e31ff60aa97350
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-29 12:24:43 +0530

    add chat page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,7 +1,24 @@
+import { io } from 'socket.io-client'
+import { useState, useEffect } from 'react'
+import { MsgBox } from './MsgBox'
+import { useLocation } from 'react-router-dom'
+
 export function Chat() {
+  const location = useLocation()
+  const [userName, setUserName] = useState(location.pathname.split('/')[2])
+  console.log(userName)
+
+  useEffect(() => {
+    const socket = io.connect('http://localhost:3000')
+    socket.on('connect', () => {
+      console.log('new user connected with id:', socket.id)
+    })
+  }, [])
+
   return (
     <div id="chat-page">
-      <h3>Chat Room</h3>
+      <h3>Chat</h3>
+      <MsgBox />
     </div>
   )
 }
