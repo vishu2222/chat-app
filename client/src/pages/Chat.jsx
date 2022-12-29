af2601c6f60a6e46cd9064997b6645d6c5f2c336
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-29 18:38:20 +0530

    add socketio

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,14 +2,17 @@ import { io } from 'socket.io-client'
 import { useState, useEffect } from 'react'
 import { MsgBox } from './MsgBox'
 import { useLocation } from 'react-router-dom'
+import { useSocket } from '../hooks/useSocket'
+
+const socket = io.connect('http://localhost:3000')
 
 export function Chat() {
   const location = useLocation()
   const [userName, setUserName] = useState(location.pathname.split('/')[2])
+
   console.log(userName)
 
   useEffect(() => {
-    const socket = io.connect('http://localhost:3000')
     socket.on('connect', () => {
       console.log('new user connected with id:', socket.id)
     })
@@ -17,7 +20,7 @@ export function Chat() {
 
   return (
     <div id="chat-page">
-      <h3>Chat</h3>
+      <h3>Chat here</h3>
       <MsgBox />
     </div>
   )
