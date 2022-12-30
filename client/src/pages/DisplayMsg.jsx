aa55f7f1800ff3ccd22fed85aeb95521cf631101
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-30 13:17:50 +0530

    render messages using messageItem component

--- client/src/pages/DisplayMsg.jsx
+++ client/src/pages/DisplayMsg.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
+import { MessageItem } from './MsgItem'
 
 export function DisplayMsg({ roomMessages }) {
   const [messages, setMessages] = useState([])
@@ -12,7 +13,7 @@ export function DisplayMsg({ roomMessages }) {
   }, [roomMessages])
 
   useEffect(() => {
-    // messages.forEach((item) => console.log(item.msg_txt))
+    messages.forEach((item) => console.log(item))
   }, [messages])
 
   const msgElements = messages.map((msg, index) => (
@@ -23,7 +24,10 @@ export function DisplayMsg({ roomMessages }) {
 
   return (
     <div>
-      <ul>{msgElements}</ul>
+      {/* <ul>{msgElements}</ul> */}
+      {messages.map((msg, index) => (
+        <MessageItem key={index} msg={msg} />
+      ))}
     </div>
   )
 }
