769314fee62b6b3f7933277ecb97a251212567a9
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-30 13:43:27 +0530

    removed unused variable msgElements

--- client/src/pages/DisplayMsg.jsx
+++ client/src/pages/DisplayMsg.jsx
@@ -9,22 +9,14 @@ export function DisplayMsg({ roomMessages }) {
     if (roomMessages !== undefined) {
       setMessages(() => roomMessages)
     }
-    console.log(messages, Array.isArray(messages))
   }, [roomMessages])
 
   useEffect(() => {
     messages.forEach((item) => console.log(item))
   }, [messages])
 
-  const msgElements = messages.map((msg, index) => (
-    <div className="msg" key={index}>
-      <h5>{msg.msg_txt}</h5>
-    </div>
-  ))
-
   return (
     <div>
-      {/* <ul>{msgElements}</ul> */}
       {messages.map((msg, index) => (
         <MessageItem key={index} msg={msg} />
       ))}
