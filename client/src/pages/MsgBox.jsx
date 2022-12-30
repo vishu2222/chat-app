bcaf2274df9b258091e5ad1baa18d4a9d1c750dd
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-30 08:57:14 +0530

    useEffect fetchRooms

--- client/src/pages/MsgBox.jsx
+++ client/src/pages/MsgBox.jsx
@@ -1,25 +1,34 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
 
-export function MsgBox({ socket, userName, room = 'None' }) {
+export function MsgBox({ socket, userName, messages }) {
   const [newMessage, setNewMessage] = useState('')
-  const [messageList, setMessageList] = useState([])
+  const [rooms, setRooms] = useState([])
 
   function handleClick() {}
 
-  return (
-    <div id="messageBox">
-      <h2>You are inside Room: {room}</h2>
+  useEffect(() => {
+    const fetchedRooms = Object.keys(messages)
+    setRooms((current) => [...current, ...fetchedRooms])
+  }, [messages])
 
-      <div id="message-container">
-        <form id="msg-form" onSubmit={(e) => e.preventDefault()}>
-          <input
-            type="text"
-            placeholder="...."
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-          />
-          <button onClick={handleClick}>send</button>
-        </form>
+  return (
+    <div id="messagePage">
+      <div id="rooms">
+        <h3>Rooms</h3>
+      </div>
+      <div id="messageBox">
+        <div id="message-container">
+          <form id="msg-form" onSubmit={(e) => e.preventDefault()}>
+            <input
+              type="text"
+              placeholder="...."
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
+            <button onClick={handleClick}>send</button>
+          </form>
+        </div>
       </div>
     </div>
   )
