ec62b406023d007d7ff0cc65ed53a75ca1a60a05
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-12-30 08:56:32 +0530

    useEffect to fetchMessages

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -2,15 +2,14 @@ import { io } from 'socket.io-client'
 import { useState, useEffect } from 'react'
 import { MsgBox } from './MsgBox'
 import { useLocation } from 'react-router-dom'
-import { useSocket } from '../hooks/useSocket'
+import { getUserChatByRooms } from '../requests'
 
 const socket = io.connect('http://localhost:3000')
 
 export function Chat() {
   const location = useLocation()
   const [userName, setUserName] = useState(location.pathname.split('/')[2])
-
-  console.log(userName)
+  const [messages, setMessages] = useState({})
 
   useEffect(() => {
     socket.on('connect', () => {
@@ -18,10 +17,18 @@ export function Chat() {
     })
   }, [])
 
+  useEffect(() => {
+    const userChat = async () => {
+      const fetchedMessages = await getUserChatByRooms(userName)
+      setMessages(() => fetchedMessages)
+    }
+    userChat()
+  }, [userName])
+
   return (
     <div id="chat-page">
       <h3>Chat here</h3>
-      <MsgBox />
+      <MsgBox messages={messages} userName={userName} socket={socket} />
     </div>
   )
 }
