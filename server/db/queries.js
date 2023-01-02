26fe3be48fa3310c38778f2b8c50161610ae43be
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-03 00:57:30 +0530

    getting users chat by room id's

--- server/db/queries.js
+++ server/db/queries.js
@@ -12,12 +12,10 @@ const pool = new Pool({
 export async function checkUserNameExists(userName) {
   const client = await pool.connect()
   const res = await client.query(
-    `SELECT user_name from users WHERE user_name ='${userName}';`
+    `SELECT user_name from users WHERE user_name ='${userName}';` // change the query
   )
   client.release()
-  if (res.rowCount > 0) {
-    return true
-  }
+  if (res.rowCount === 1) return true
   return false
 }
 
@@ -25,65 +23,64 @@ export async function signUp(userName, password) {
   const client = await pool.connect()
   const res =
     await client.query(`INSERT INTO users(user_id, user_name, password) 
-    VALUES (NEXTVAL('user_id_seq'), '${userName}', '${password}')`)
+    VALUES (NEXTVAL('user_id_seq'), '${userName}', '${password}')`) //
   client.release()
-  if (res.rowCount < 1) throw Error
+  if (res.rowCount < 1) throw Error //
 }
 
 export async function getPassword(userName) {
   const client = await pool.connect()
   const res = await client.query(
-    `SELECT password FROM users where user_name = '${userName}'`
+    `SELECT password FROM users where user_name = '${userName}'` //
   )
   client.release()
   return await res.rows[0].password
 }
 
-async function getUserRooms(userName) {
+async function getUserId(userName) {
   const client = await pool.connect()
-  const userRooms = `SELECT DISTINCT room 
-                     FROM 
-                        ( 
-                        SELECT room 
-                        FROM conversations, rooms, users 
-                        WHERE conversations.sender_id=users.user_id 
-                        AND conversations.room_id=rooms.room_id 
-                        AND users.user_name='${userName}'
-                        )  AS filteredTable; `
-  let res = await client.query(userRooms)
-  const roomsList = res.rows.map((item) => item.room)
+  const query = `SELECT user_id FROM users WHERE user_name = '${userName}'`
+  const res = await client.query(query)
+  if (res.rows.length === 0) return null
   client.release()
-  return roomsList
+  return res.rows[0].user_id
 }
 
-async function getRoomChat(room) {
+async function getRoomsList(user_id) {
   const client = await pool.connect()
-  const roomChat = `SELECT user_name, room, msg_txt, msg_time 
-    FROM conversations, rooms, users
-    WHERE conversations.sender_id=users.user_id 
-    AND conversations.room_id=rooms.room_id 
-    AND rooms.room = '${room}'
-    ORDER BY msg_time ASC LIMIT 10;`
+  const query = `SELECT room_id
+                 FROM userrooms
+                 WHERE user_id=${user_id};`
+  const res = await client.query(query)
+  if (res.rows.length === 0) return []
+  client.release()
+  return res.rows.map((item) => item.room_id)
+}
+
+async function getRoomChat(room_id) {
+  const client = await pool.connect()
+  const roomChat = `SELECT  msg_txt, msg_time, user_name, room_name
+                    FROM messages, users, rooms
+                    WHERE messages.room_id = '${room_id}'
+                    AND messages.sender_id = users.user_id
+                    AND rooms.room_id = messages.room_id
+                    ORDER BY msg_time ASC LIMIT 10;`
   const res = await client.query(roomChat)
   client.release()
   return res.rows
 }
 
-export async function getUserChatByRooms(userName) {
+export async function getUsersChatByRoom(userName) {
+  const client = await pool.connect()
+  const id = await getUserId(userName)
+
+  if (id === null) return 404
+  const roomsList = await getRoomsList(id)
+
   const userMsgsByRoom = {}
-  const roomsList = await getUserRooms(userName)
-  for (let room of roomsList) {
-    userMsgsByRoom[room] = await getRoomChat(room)
+  for (const room_id of roomsList) {
+    userMsgsByRoom[room_id] = await getRoomChat(room_id)
   }
+  client.release()
   return userMsgsByRoom
 }
-
-// console.log(await getUserChatByRooms('aaaa'))
-
-// INSERT INTO rooms(room_id,room) VALUES ((nextval('room_id_seq')),'demo');
-
-// msgs
-// select user_name, room, msg_txt, msg_time from conversations, rooms, users where conversations.sender_id=users.user_id and conversations.room_id=rooms.room_id and users.user_name='aaaa';
-
-// distinct rooms
-// SELECT DISTINCT room from (SELECT user_name, room, msg_txt, msg_time FROM conversations, rooms, users WHERE conversations.sender_id=users.user_id AND conversations.room_id=rooms.room_id AND users.user_name='aaaa') AS filteredTable;
