6764d0e55eaa1c14ca95851984f7f9b22c502a3b
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-03 14:36:26 +0530

    remove unused imort checkUserNameExist

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
-import { userLogin, checkUserNameExists } from '../requests.js'
+import { userLogin } from '../requests.js'
 
 export function Login() {
   const [userName, setUserName] = useState('')
