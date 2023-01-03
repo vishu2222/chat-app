5eecf9814c82d7766046bedc8cec4d8c6713c5f6
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-03 09:59:18 +0530

    refactor

--- server/jwt.js
+++ server/jwt.js
@@ -3,11 +3,8 @@ import jwt from 'jsonwebtoken'
 export async function signJwt(data, key) {
   return new Promise((resolve, reject) => {
     jwt.sign(data, key, (err, token) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(token)
-      }
+      if (err) reject(err)
+      else resolve(token)
     })
   })
 }
@@ -15,11 +12,8 @@ export async function signJwt(data, key) {
 export async function verifyJwt(token, secretKey) {
   return new Promise((resolve, reject) => {
     jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(user)
-      }
+      if (err) reject(err)
+      else resolve(user)
     })
   })
 }
