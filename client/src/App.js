ff2f9738dfca3485ae818c2d7edba088d16f81a0
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-05 16:01:51 +0530

    add navigation bar on top and logout route

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
 import { Chat } from './pages/Chat'
 import { Home } from './pages/Home'
+import { Logout } from './pages/Logout'
 
 function App() {
   return (
     <>
       <div id='appContainer'>
         <BrowserRouter>
+          <div id='Nav-div'>
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Contact</Link>
+            <Link to='/logout'>logOut</Link>
+          </div>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/chat/:userName' element={<Chat />} />
+            <Route path='/chat' element={<Chat />} />
+            <Route path='/logout' element={Logout} />
             <Route path='*' element={<h1>Page not found</h1>} />
           </Routes>
         </BrowserRouter>
