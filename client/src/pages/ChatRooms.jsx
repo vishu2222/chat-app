cff1d0ffa513c6172b2216d8aede2bb046d54e08
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-08 16:25:25 +0530

    add function roomsList to get roomsList

--- client/src/pages/ChatRooms.jsx
+++ client/src/pages/ChatRooms.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from 'react'
+import { getRooms } from '../requests.js'
+
 export function ChatRooms() {
-  return <div id='chatRooms'></div>
+  useEffect(() => {
+    async function roomsList() {
+      const rooms = await getRooms()
+      console.log(rooms)
+    }
+    roomsList()
+  }, [])
+
+  return (
+    <div id='chatRooms'>
+      <p>Welcome {localStorage.getItem('userName')}</p>
+      <div id='rooms'></div>
+      <div id='messages-container'></div>
+    </div>
+  )
 }
