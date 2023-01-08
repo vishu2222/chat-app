7475e0736dd0303beff24f26d868fbf50aca6e91
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-08 14:27:35 +0530

    rename function checkUserNameExists

--- server/models/queries.js
+++ server/models/queries.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 const client = await pool.connect() // may not handle idle clients
 
 //  checkUserNameExists
-export async function checkUserNameExists(userName) {
+export async function isUserNameAvailable(userName) {
   const res = await client.query('SELECT user_name from users WHERE user_name = $1', [userName])
   if (res.rowCount === 1) return true
   return false
@@ -46,40 +46,40 @@ async function getRoomsList(user_id) {
   return res.rows.map((item) => item.room_id)
 }
 
-async function getRoomChat(room_id) {
-  const roomChat = `SELECT  msg_txt, msg_time, user_name, room_name
-                    FROM messages, users, rooms
-                    WHERE messages.room_id = '${room_id}'
-                    AND messages.sender_id = users.user_id
-                    AND rooms.room_id = messages.room_id
-                    ORDER BY msg_time ASC LIMIT 10;`
-  const res = await client.query(roomChat)
-  return res.rows
-}
+// async function getRoomChat(room_id) {
+//   const roomChat = `SELECT  msg_txt, msg_time, user_name, room_name
+//                     FROM messages, users, rooms
+//                     WHERE messages.room_id = '${room_id}'
+//                     AND messages.sender_id = users.user_id
+//                     AND rooms.room_id = messages.room_id
+//                     ORDER BY msg_time ASC LIMIT 10;`
+//   const res = await client.query(roomChat)
+//   return res.rows
+// }
 
-export async function getChatByRoom(userName) {
-  const id = await getUserId(userName)
+// export async function getChatByRoom(userName) {
+//   const id = await getUserId(userName)
 
-  if (id === null) return 404
-  const roomsList = await getRoomsList(id)
+//   if (id === null) return 404
+//   const roomsList = await getRoomsList(id)
 
-  const userMsgsByRoom = {}
-  for (const room_id of roomsList) {
-    userMsgsByRoom[room_id] = await getRoomChat(room_id)
-  }
-  return userMsgsByRoom
-}
+//   const userMsgsByRoom = {}
+//   for (const room_id of roomsList) {
+//     userMsgsByRoom[room_id] = await getRoomChat(room_id)
+//   }
+//   return userMsgsByRoom
+// }
 
 export async function addMsg(msg) {
-  const userId = await getUserId(msg.user_name)
+  // const userId = await getUserId(msg.user_name)
   // client.query(
   //   "INSERT INTO messages (msg_id, msg_txt, msg_time, sender_id, room_id) VALUES (nextval('msg_id_seq'), $1, $2, $3, $4)",
   //   [msg.msg_txt, msg.msg_time, userId, msg.roomId]
   // )
-  client.query(
-    `INSERT INTO messages (msg_id, msg_txt, msg_time, sender_id, room_id)
-     VALUES (nextval('msg_id_seq'), '${msg.msg_txt}', to_timestamp(${msg.msg_time}), '${userId}', '${msg.roomId}' );`
-  )
+  // client.query(
+  //   `INSERT INTO messages (msg_id, msg_txt, msg_time, sender_id, room_id)
+  //    VALUES (nextval('msg_id_seq'), '${msg.msg_txt}', to_timestamp(${msg.msg_time}), '${userId}', '${msg.roomId}' );`
+  // )
 }
 
 // client.query(
