fc518cb5a3e9de60d983b899f098bafc7f8101ba
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-08 12:33:16 +0530

    import chatrooms

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,8 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Register } from './pages/Register'
-import { Chat } from './pages/Chat'
+import { ChatRooms } from './pages/ChatRooms'
 import { Home } from './pages/Home'
-import { Logout } from './pages/Logout'
 
 function App() {
   return (
@@ -14,14 +13,13 @@ function App() {
           <div id='Nav-div'>
             <Link to='/'>Home</Link>
             <Link to='/login'>login</Link>
-            {/* <Link to='/logout'>logOut</Link> */}
+            <Link to='/register'>register</Link>
           </div>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/chat' element={<Chat />} />
-            <Route path='/logout' element={Logout} />
+            <Route path='/chatRooms' element={<ChatRooms />} />
             <Route path='*' element={<h1>Page not found</h1>} />
           </Routes>
         </BrowserRouter>
