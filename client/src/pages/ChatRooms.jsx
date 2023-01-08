cf4766d064aa3fef1d57d5934a07370eb96ab2c8
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-08 17:06:59 +0530

    add functions generalRoomMsgs and roomsList

--- client/src/pages/ChatRooms.jsx
+++ client/src/pages/ChatRooms.jsx
@@ -1,20 +1,35 @@
-import { useEffect } from 'react'
-import { getRooms } from '../requests.js'
+import { useState, useEffect } from 'react'
+import { getRooms, getGeneralRoomMsgs } from '../requests.js'
 
 export function ChatRooms() {
+  const [roomsList, setRoomsList] = useState([])
+  const [messages, setMessages] = useState([])
+
   useEffect(() => {
     async function roomsList() {
       const rooms = await getRooms()
-      console.log(rooms)
+      setRoomsList(() => rooms)
     }
+
+    async function generalRoomMsgs() {
+      const generalMsgs = await getGeneralRoomMsgs()
+      setMessages(() => generalMsgs)
+    }
+
     roomsList()
+    generalRoomMsgs()
   }, [])
+
+  useEffect(() => {
+    console.log('roomsList:', roomsList, 'messages:', messages)
+  }, [roomsList, messages])
 
   return (
     <div id='chatRooms'>
       <p>Welcome {localStorage.getItem('userName')}</p>
       <div id='rooms'></div>
       <div id='messages-container'></div>
+      <div id='join-room'></div>
     </div>
   )
 }
