319b2afee1b6f8933c9887a81616cd962f4fc527
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-09 15:05:22 +0530

    import styles and add component msgForm

--- client/src/pages/MessageContainer.jsx
+++ client/src/pages/MessageContainer.jsx
@@ -1,16 +1,19 @@
+import '../styles/MessageContainer.css'
 import { AppContext } from './ChatRooms'
 import { useContext } from 'react'
 import { MessageItem } from './MessageItem'
+import { MsgForm } from './MsgForm'
 
 export function MessageContainer() {
   const { messages } = useContext(AppContext)
-  //   console.log('messages', messages)
+
   return (
     <div id='message-container'>
       <p>messge-container</p>
       {messages.map((msg) => (
         <MessageItem key={msg.msg_id} msg={msg} />
       ))}
+      <MsgForm />
     </div>
   )
 }
