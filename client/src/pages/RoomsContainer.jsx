8724b0ed0b356c9f932931edb7535ba1f9771104
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-09 15:06:03 +0530

    renamed displayrooms to roomscontainer

---
+++ client/src/pages/RoomsContainer.jsx
@@ -0,0 +1,32 @@
+import { AppContext } from './ChatRooms'
+import { useContext, useState } from 'react'
+import { getRoomMsgs } from '../requests.js'
+import '../styles/RoomsContainer.css'
+
+export function RoomsContainer() {
+  const [focusedRoomId, setFocusedRoomId] = useState(1)
+  const { roomsList, setMessages } = useContext(AppContext)
+
+  async function focusRoom(roomId) {
+    if (focusedRoomId !== roomId) {
+      const roomMsgs = await getRoomMsgs(roomId)
+      setMessages(() => roomMsgs)
+      setFocusedRoomId(() => roomId)
+    }
+  }
+
+  const roomElements = roomsList.map((room) => (
+    <div className='roomItem' key={room.room_id} onClick={() => focusRoom(room.room_id)}>
+      <p className='room-content'>{room.room_name}</p>
+    </div>
+  ))
+
+  return (
+    <div id='rooms-container'>
+      <div id='rooms-title'>
+        <p>Rooms</p>
+      </div>
+      <div className='room-items'>{roomElements}</div>
+    </div>
+  )
+}
