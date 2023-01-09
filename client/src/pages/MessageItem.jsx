22be9df557df1588d788ecdfe59cd9ab001afaf6
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-09 10:54:20 +0530

    add componetn messageItem

---
+++ client/src/pages/MessageItem.jsx
@@ -0,0 +1,20 @@
+export function MessageItem({ msg }) {
+  const userName = localStorage.getItem('userName')
+  console.log(msg)
+  let divClassName = ''
+  let msgClassName = ''
+
+  if (msg.user_name === userName) {
+    divClassName = 'self-msg-div'
+    msgClassName = 'self-msg'
+  } else {
+    divClassName = 'user-msg-div'
+    msgClassName = 'user-msg'
+  }
+
+  return (
+    <div className={divClassName}>
+      <p className={msgClassName}>{msg.user_name + ': ' + msg.msg_txt}</p>
+    </div>
+  )
+}
