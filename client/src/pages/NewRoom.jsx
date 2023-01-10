222e57b475c096fa118fe94ef9135e1b6bc9aa00
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-10 12:07:08 +0530

    add function createRoom to handle click

--- client/src/pages/NewRoom.jsx
+++ client/src/pages/NewRoom.jsx
@@ -1,29 +1,62 @@
 import { useState } from 'react'
 import '../styles/NewRoom.css'
 import { joinUser } from '../requests'
+import { createNewRoom } from '../requests'
 
 export function NewRoom({ socket }) {
   const [room, setRoom] = useState('')
+  const [newRoom, setNewRoom] = useState('')
+  const [displayErr, setDisplayErr] = useState(false)
+  const [errMsg, setErrMsg] = useState('')
 
   function joinRoom() {
     joinUser(room)
     setRoom('')
   }
 
-  function createRoom() {}
+  async function createRoom() {
+    const res = await createNewRoom(newRoom)
+    if (res === 403) {
+      setErrMsg('room name already exists')
+      setDisplayErr(true)
+    }
+    if (res === 500) {
+      setErrMsg('server error')
+      setDisplayErr(true)
+    }
+    setNewRoom('')
+  }
 
   return (
     <div id='join-room'>
       <label>
         <h4>Join a Room</h4>
       </label>
-      <input type='text' placeholder='Room' onChange={(e) => setRoom(e.target.value)} />
+      <input
+        type='text'
+        placeholder='Room'
+        value={room}
+        onChange={(e) => {
+          setRoom(e.target.value)
+          setDisplayErr(false)
+        }}
+      />
       <button onClick={joinRoom}>Join</button>
 
       <label>
         <h4>Create New Room</h4>
       </label>
-      <input type='text' placeholder='create' onChange={(e) => createRoom(e.target.value)} />
+      <input
+        type='text'
+        placeholder='create'
+        value={newRoom}
+        onChange={(e) => {
+          setNewRoom(e.target.value)
+          setDisplayErr(false)
+        }}
+      />
+      <button onClick={createRoom}>Create room</button>
+      {displayErr && <h3 className='err-msg'>{errMsg}</h3>}
     </div>
   )
 }
