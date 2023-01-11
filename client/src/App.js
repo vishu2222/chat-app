8b37f4b32c813a0a969f59fe058fe459795024fb
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-11 10:35:49 +0530

    remove logout component and add signout function

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,12 @@ import { Login } from './pages/Login'
 import { Register } from './pages/Register'
 import { ChatRooms } from './pages/ChatRooms'
 import { Home } from './pages/Home'
-import { Logout } from './pages/LogOut'
+import { signOut } from './requests'
 
 function App() {
+  async function logout() {
+    await signOut()
+  }
   return (
     <>
       <div id='appContainer'>
@@ -18,7 +21,7 @@ function App() {
             <Link to='/login' className='nav-links'>
               Login
             </Link>
-            <Link to='/logout' className='nav-links'>
+            <Link to='/login' className='nav-links' onClick={logout}>
               LogOut
             </Link>
           </div>
@@ -34,7 +37,7 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/chatRooms' element={<ChatRooms />} />
-            <Route path='/logout' element={<Logout />} />
+            {/* <Route path='/logout' element={<Logout />} /> */}
             <Route path='*' element={<h1>Page not found</h1>} />
           </Routes>
         </BrowserRouter>
