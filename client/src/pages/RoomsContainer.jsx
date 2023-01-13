c09c8bacb4f61ee6a3ced9bc03eb3abc1f123eab
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-13 10:03:36 +0530

    update focusedRoomName state

--- client/src/pages/RoomsContainer.jsx
+++ client/src/pages/RoomsContainer.jsx
@@ -5,7 +5,8 @@ import '../styles/RoomsContainer.css'
 
 export function RoomsContainer() {
   const [focusedRoomId, setFocusedRoomId] = useState(1)
-  const { roomsList, setMessages, socket } = useContext(AppContext)
+
+  const { roomsList, setMessages, socket, setFocusedRoomName } = useContext(AppContext)
 
   useEffect(() => {
     socket.on('user-joined', (msg) => {
@@ -25,18 +26,20 @@ export function RoomsContainer() {
     }
   }, [socket, focusedRoomId])
 
-  async function focusRoom(roomId) {
+  async function focusRoom(roomId, roomName) {
     if (focusedRoomId !== roomId) {
       socket.emit('leave-room', focusedRoomId)
       socket.emit('join-room', roomId)
+
       const roomMsgs = await getRoomMsgs(roomId)
       setMessages(() => roomMsgs)
       setFocusedRoomId(() => roomId)
+      setFocusedRoomName(() => roomName)
     }
   }
 
   const roomElements = roomsList.map((room) => (
-    <div className='roomItem' key={room.room_id} onClick={() => focusRoom(room.room_id)}>
+    <div className='roomItem' key={room.room_id} onClick={() => focusRoom(room.room_id, room.room_name)}>
       <p className='room-content'>{room.room_name}</p>
     </div>
   ))
@@ -44,7 +47,9 @@ export function RoomsContainer() {
   return (
     <div id='rooms-container'>
       <div id='rooms-title-div'>
-        <p id='rooms-title'>Rooms</p>
+        <p id='rooms-title'>
+          <strong>Rooms</strong>
+        </p>
       </div>
       <div className='room-items'>{roomElements}</div>
     </div>
