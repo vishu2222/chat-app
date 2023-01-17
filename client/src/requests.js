72d676cf3036fb05a927cafafc1d50df633a4d68
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-01-17 08:49:19 +0530

    change urls to rest standards

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,13 +1,13 @@
 const url = 'http://localhost:3000/'
 
 export async function checkUserNameAvailable(userName) {
-  const res = await fetch(url + 'is-available/' + userName)
+  const res = await fetch(url + 'users/' + userName + '/check')
   const response = await res.json()
-  return response.status
+  return response.available
 }
 
 export async function signupUser(userName, password) {
-  const res = await fetch(url + 'sign-up', {
+  const res = await fetch(url + 'users/sign-up', {
     headers: { 'Content-Type': 'application/json' },
     method: 'POST',
     body: JSON.stringify({ userName, password })
@@ -16,14 +16,14 @@ export async function signupUser(userName, password) {
 }
 
 export async function authenticateUser() {
-  const res = await fetch(url + 'authenticate-user', {
+  const res = await fetch(url + 'users/me', {
     credentials: 'include'
   })
   return res.status
 }
 
 export async function signIn(userName, password) {
-  const res = await fetch(url + 'login', {
+  const res = await fetch(url + 'users/login', {
     credentials: 'include',
     headers: { 'Content-Type': 'application/json' },
     method: 'POST',
@@ -33,7 +33,7 @@ export async function signIn(userName, password) {
 }
 
 export async function signOut() {
-  const res = await fetch(url + 'logout', {
+  const res = await fetch(url + 'users/logout', {
     credentials: 'include',
     headers: { 'Content-Type': 'application/json' },
     method: 'POST'
@@ -42,12 +42,12 @@ export async function signOut() {
 }
 
 export async function getRooms() {
-  const res = await fetch(url + 'rooms-list', { credentials: 'include' })
+  const res = await fetch(url + 'rooms/list', { credentials: 'include' })
   return await res.json()
 }
 
 export async function getGeneralRoomMsgs() {
-  const res = await fetch(url + 'default-room-msgs', { credentials: 'include' })
+  const res = await fetch(url + 'msgs/default-room', { credentials: 'include' })
   return await res.json()
 }
 
@@ -57,7 +57,7 @@ export async function getRoomMsgs(roomId) {
 }
 
 export async function joinUser(room) {
-  const res = await fetch(url + 'join-room', {
+  const res = await fetch(url + 'rooms/join-room', {
     credentials: 'include',
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -68,7 +68,7 @@ export async function joinUser(room) {
 }
 
 export async function createNewRoom(room) {
-  const res = await fetch(url + 'create-room', {
+  const res = await fetch(url + 'rooms/create-room', {
     credentials: 'include',
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
