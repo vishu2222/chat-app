bf0ee05ee5ab712e2f61055f8ff12667d04dc2a7
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-04-16 18:50:07 +0530

    remove console logs

--- server/utilities/middlewere.js
+++ server/utilities/middlewere.js
@@ -6,7 +6,6 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export async function authenticateToken(req, res, next) {
   const token = req.cookies.token
-  console.log(req.path)
   if (req.path === '/login' || req.path.endsWith('check') || req.path.endsWith('sign-up')) {
     next()
     return
@@ -14,7 +13,7 @@ export async function authenticateToken(req, res, next) {
   if (token === undefined) return res.sendStatus(401)
   try {
     const jwtPaylod = await verifyJwt(token, process.env.SECRET_KEY)
-    res.userId = jwtPaylod.user_id // attach to req
+    res.userId = jwtPaylod.user_id // attach to req instead
     next()
   } catch (err) {
     return res.sendStatus(401)
