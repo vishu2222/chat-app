41238fb2a2358865cab9cd0b749ea425e882578c
vishu2222 <vishwanathreddyaenugu@gmail.com>
2023-04-16 18:45:54 +0530

    append baseurl with /api

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000/'
+const url = 'http://localhost:3000/api/'
 
 export async function checkUserNameAvailable(userName) {
   const res = await fetch(url + 'users/' + userName + '/check')
